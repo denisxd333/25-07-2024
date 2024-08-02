05d3f33481cd2f1d63c00e2fe3a7d2377f4c798d
denisxd333 <deniscardona637@gmail.com>
2024-08-02 16:10:51 -0600

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,12 @@
+//Corregir este codigo para que imprima la suma de a y b
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    a = 5;
+    b = 10;
+    printf("%d + %d = %d\n", a, b, a + b);
+    return 0;
+}   
